b5163846045ac8de4b2e28c8881232852c509a87
calvincetom <calvincetom@outlook.com>
2024-10-25 10:39:32 +0300

    remove unavailable components

--- components/ui/dashboard/VendorSelect.tsx
+++ components/ui/dashboard/VendorSelect.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import Vendor from "./Vendor";
-import Topnav from "./Topnav";
 
 const VendorSelectPage = () => {
   return (
     <div className="w-full min-h-screen bg-green-50 flex flex-col items-center p-2">
       {/* Header Section */}
-      <Topnav />
 
       {/* Main Content */}
       <div className="w-full container text-left">
